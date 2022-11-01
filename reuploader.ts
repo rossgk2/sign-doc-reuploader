6eec84f4c6f166fcb070c96fc112486895a471f4
Ross Grogan-Kaylor <rossgk2@gmail.com>
2022-11-01 13:23:54 -0400

    Fixed uploading error
     The problem was that values of string parameters that are sent to the API have to be wrapped in double quotes; e.g. you have to send '"this_is_a_file_name"' instead of 'this_is_a_file_name'.

--- reuploader.ts
+++ reuploader.ts
@@ -1,8 +1,8 @@
 const
-	fs = require('fs'),
-	axios = require('axios'),
-	FormData = require('form-data'), // https://maximorlov.com/send-a-file-with-axios-in-nodejs/
-	FileSaver = require('file-saver'); // https://github.com/eligrey/FileSaver.js/
+  fs = require('fs'),
+  axios = require('axios'),
+  FormData = require('form-data'), // https://maximorlov.com/send-a-file-with-axios-in-nodejs/
+  FileSaver = require('file-saver'); // https://github.com/eligrey/FileSaver.js/
 
 import {Blob} from 'buffer';
 
@@ -13,146 +13,147 @@ const defaultRequestConfig = { 'headers' : defaultHeadersConfig };
 /* Retrieves the base URI that is the first part of all Adobe Sign API endpoints. */
 async function getBaseUri()
 {
-	let response = await axios.get('https://api.na1.adobesign.com:443/api/rest/v6/baseUris', defaultRequestConfig);
-	let baseUri = response.data['apiAccessPoint'];
-	baseUri = baseUri.substring(0, baseUri.length - 1) + "/api/rest/v6";
-	return baseUri;
+  let response = await axios.get('https://api.na1.adobesign.com:443/api/rest/v6/baseUris', defaultRequestConfig);
+  let baseUri = response.data['apiAccessPoint'];
+  baseUri = baseUri.substring(0, baseUri.length - 1) + "/api/rest/v6";
+  return baseUri;
 }
 
 /* 
-	Downloads the content located at url and stores it at the relative path (with the root directory
-	being the one that contains this file) that is dest.
+  Downloads the content located at url and stores it at the relative path (with the root directory
+  being the one that contains this file) that is dest.
 
-	Informed by https://github.com/axios/axios/issues/3971#issuecomment-1159556428.
+  Informed by https://github.com/axios/axios/issues/3971#issuecomment-1159556428.
 */
 async function download(url: string, dest: string, cb: () => (void))
 {
-	let file = fs.createWriteStream(dest);
-	file.on('finish', function() { file.close(cb); });	
-	let {data} = await axios.get(url, {'responseType': 'stream'});
-	data.pipe(file); // this wouldn't work if we didn't use 'responseType' : 'stream'
+  let file = fs.createWriteStream(dest);
+  file.on('finish', function() { file.close(cb); });  
+  let {data} = await axios.get(url, {'responseType': 'stream'});
+  data.pipe(file); // this wouldn't work if we didn't use 'responseType' : 'stream'
 }
 
 function printWithEqualsSep(message: string)
 {
-	console.log(message);
-	console.log("================================================");
+  console.log(message);
+  console.log("================================================");
 }
 
 /* Returns the JSON that specifies custom headers for an HTTP request. */
 function getRequestConfig(form: typeof FormData)
 {
-	return { 'headers' : {...defaultHeadersConfig, ...form.getHeaders()} }
+  return { 'headers' : {...defaultHeadersConfig, ...form.getHeaders()} }
 }
 
 async function main(libraryDocumentId: string, debug: boolean)
 {
-	let baseUri = await getBaseUri();
+  let baseUri = await getBaseUri();
 
-	/* GET the name of the document. */
-	let docInfo = await axios.get(`${baseUri}/libraryDocuments/${libraryDocumentId}`, defaultRequestConfig);
-	let docName = docInfo.data.name;
+  /* GET the name of the document. */
+  let docInfo = await axios.get(`${baseUri}/libraryDocuments/${libraryDocumentId}`, defaultRequestConfig);
+  let docName = docInfo.data.name;
 
-	/* GET the values the user has entered into the document's fields. */
-	let formFields = await axios.get(`${baseUri}/libraryDocuments/${libraryDocumentId}/formFields`, defaultRequestConfig);
-	formFields = formFields.data;
+  /* GET the values the user has entered into the document's fields. */
+  let formFields = await axios.get(`${baseUri}/libraryDocuments/${libraryDocumentId}/formFields`, defaultRequestConfig);
+  formFields = formFields.data;
 
-	if (debug)
-		printWithEqualsSep(formFields);
+  if (debug)
+    printWithEqualsSep(formFields);
 
-	/* GET the PDF on which the custom form fields that the user field out were placed.*/
-	let combinedDocumentUrl = await axios.get(`${baseUri}/libraryDocuments/${libraryDocumentId}/combinedDocument/url`, defaultRequestConfig);
-	combinedDocumentUrl = combinedDocumentUrl.data.url;
+  /* GET the PDF on which the custom form fields that the user field out were placed.*/
+  let combinedDocumentUrl = await axios.get(`${baseUri}/libraryDocuments/${libraryDocumentId}/combinedDocument/url`, defaultRequestConfig);
+  combinedDocumentUrl = combinedDocumentUrl.data.url;
 
-	/* Save the PDF to the folder this script resides in. */
-	const savedFileName = 'combined_document.pdf';
-	await download(combinedDocumentUrl, savedFileName, function() { console.log("Download completed."); });	
+  /* Save the PDF to the folder this script resides in. */
+  const savedFileName = 'combined_document.pdf';
+  await download(combinedDocumentUrl, savedFileName, function() { console.log("Download completed."); }); 
 
-	/* POST the same document (but without any custom form fields) as a transient document and get its ID.
-	
-	Informed by https://stackoverflow.com/questions/53038900/nodejs-axios-post-file-from-local-server-to-another-server. */
-	let form = new FormData();
-	form.append('File-Name', docName);
-	form.append('File', fs.createReadStream(savedFileName));
-	let response = await axios.post(`${baseUri}/transientDocuments`, form, getRequestConfig(form));
-	let transientDocumentId = response.data.transientDocumentId;
+  /* POST the same document (but without any custom form fields) as a transient document and get its ID.
+  
+  Informed by https://stackoverflow.com/questions/53038900/nodejs-axios-post-file-from-local-server-to-another-server. */
+  let form = new FormData();
+  form.append('File-Name', `"${docName}"`); // have to enclose values for File-Name and File in double quotes 
+  form.append('File', `"${fs.createReadStream(savedFileName)}"`);
+  let response = await axios.post(`${baseUri}/transientDocuments`, form, getRequestConfig(form));
+  let transientDocumentId = response.data.transientDocumentId;
 
-	if (debug)
-	{
-		printWithEqualsSep(response.data);
-		console.log(`Status code of response to POST to /transientDocuments: ${response.status}`);
-	}
+  if (debug)
+  {
+    printWithEqualsSep(response.data);
+    console.log(`Status code of response to POST to /transientDocuments: ${response.status}`);
+  }
 
-	/* Create a library document from the just-created transient document. */
-	let libraryDocumentInfo =	
-	{
-		'fileInfos' : [{'transientDocumentId' : transientDocumentId}],
-		'name': savedFileName,
-		'sharingMode': 'ACCOUNT', // can be 'USER' or 'GROUP' or 'ACCOUNT' or 'GLOBAL'
-		'state': 'AUTHORING', // can be 'AUTHORING' or 'ACTIVE'
-		'templateTypes': ['DOCUMENT'] // each array elt can be 'DOCUMENT' or 'FORM_FIELD_LAYER'
-	};
+  /* Create a library document from the just-created transient document. */
+  let libraryDocumentInfo = 
+  {
+    'fileInfos' : [{'transientDocumentId' : transientDocumentId}],
+    'name': savedFileName,
+    'sharingMode': 'ACCOUNT', // can be 'USER' or 'GROUP' or 'ACCOUNT' or 'GLOBAL'
+    'state': 'AUTHORING', // can be 'AUTHORING' or 'ACTIVE'
+    'templateTypes': ['DOCUMENT'] // each array elt can be 'DOCUMENT' or 'FORM_FIELD_LAYER'
+  };
 
-	/* IN PROGRESS */
+  /* IN PROGRESS */
 
-	let approachSelector = 1;
+  let approachSelector = 1;
 
-	/* Approach 1: send JSON.stringify(libraryDocumentInfo) as the request body and specify
-	a header of 'Content-Type' : 'application/json'. */
+  /* Approach 1: send JSON.stringify(libraryDocumentInfo) as the request body and specify
+  a header of 'Content-Type' : 'application/json'. */
 
-	if (approachSelector === 1)
-	{
-		let headersConfig = { 'headers' : { ...defaultHeadersConfig, 'Content-Type' : 'application/json' } };
-		response = await axios.post(`${baseUri}/libraryDocuments`, JSON.stringify(libraryDocumentInfo), headersConfig);
-	
-		/* The response body to this is "{code: 'MISC_SERVER_ERROR', message: 'Some miscellaneous error has occurred'}". */
+  if (approachSelector === 1)
+  {
+    let headersConfig = { 'headers' : { ...defaultHeadersConfig, 'Content-Type' : 'application/json' } };
+    response = await axios.post(`${baseUri}/libraryDocuments`, JSON.stringify(libraryDocumentInfo), headersConfig);
+  
+    /* The response body to this is "{code: 'MISC_SERVER_ERROR', message: 'Some miscellaneous error has occurred'}". */
 
-		if (debug)
-			printWithEqualsSep(response.data);
-	}
+    if (debug)
+    {
+      console.log('Approach 1 results...');
+      printWithEqualsSep(response.data);
+    }
+  }
 
-	/* Approach 2: create a FormData object that holds libraryDocumentInfo. */
+  /* Approach 2: create a FormData object that holds libraryDocumentInfo. */
 
-	else if (approachSelector === 2)
-	{
-		let form1 = new FormData();
-		form1.append('LibraryDocumentInfo', JSON.stringify(libraryDocumentInfo));
-		let requestConfig1 = { 'headers' : {...defaultHeadersConfig, ...form1.getHeaders(), 'Content-Type' : 'application/json'} };
-		let response1 = await axios.post(`${baseUri}/libraryDocuments`, form1, requestConfig1); 
+  else if (approachSelector === 2)
+  {
+    let form1 = new FormData();
+    form1.append('LibraryDocumentInfo', JSON.stringify(libraryDocumentInfo));
+    let requestConfig1 = { 'headers' : {...defaultHeadersConfig, ...form1.getHeaders(), 'Content-Type' : 'application/json'} };
+    let response1 = await axios.post(`${baseUri}/libraryDocuments`, form1, requestConfig1); 
 
-		/* The response body to this is "{ code: 'INVALID_JSON', message: 'An invalid JSON was specified' }".
-		I've double checked and it seems to me that libraryDocumentInfo is in the correct format. */
+    /* The response body to this is "{ code: 'INVALID_JSON', message: 'An invalid JSON was specified' }".
+    I've double checked and it seems to me that libraryDocumentInfo is in the correct format. */
 
-		if (debug)
-			printWithEqualsSep(response1.data);
-	}
+    if (debug)
+      printWithEqualsSep(response1.data);
+  }
 
-	/* Approach 3: same as approach 2, but uses a Blob to specify the type of form1's content. 
-	In order to make this approach testable we first need to figure out how to convert a Blob
-	to a Buffer in NodeJS. This module may be helpful: https://github.com/feross/blob-to-buffer.*/
+  /* Approach 3: same as approach 2, but uses a Blob to specify the type of form1's content. 
+  In order to make this approach testable we first need to figure out how to convert a Blob
+  to a Buffer in NodeJS. This module may be helpful: https://github.com/feross/blob-to-buffer.*/
 
-	else if (approachSelector === 3)
-	{
-		let form1 = new FormData();
-		let blob = new Blob([JSON.stringify(libraryDocumentInfo)], {'type' : 'application/json'});
-		let blobToBuffer = function(blob: Blob) { }; // figure out some implementation for this
-		form1.append('LibraryDocumentInfo', blobToBuffer(blob));
-		
-		/* form1.append() expectedly throws an error right now since blobToBuffer() doesn't do anything. */
+  else if (approachSelector === 3)
+  {
+    let form1 = new FormData();
+    let blob = new Blob([JSON.stringify(libraryDocumentInfo)], {'type' : 'application/json'});
+    let blobToBuffer = function(blob: Blob) { }; // figure out some implementation for this
+    form1.append('LibraryDocumentInfo', blobToBuffer(blob));
+    
+    /* form1.append() expectedly throws an error right now since blobToBuffer() doesn't do anything. */
 
-		let requestConfig1 = { 'headers' : {...defaultHeadersConfig, ...form1.getHeaders()} };
-		let response1 = await axios.post(`${baseUri}/libraryDocuments`, form1, requestConfig1); 
+    let requestConfig1 = { 'headers' : {...defaultHeadersConfig, ...form1.getHeaders()} };
+    let response1 = await axios.post(`${baseUri}/libraryDocuments`, form1, requestConfig1); 
 
-		if (debug)
-			printWithEqualsSep(response1.data);
-	}
+    if (debug)
+      printWithEqualsSep(response1.data);
+  }
 
-	/* Use a PUT request to add the custom form fields and the values entered earlier to the document. */
-	
-	// await axios.put(`/libraryDocuments/${libraryDocumentId}/formFields`);
+  /* Use a PUT request to add the custom form fields and the values entered earlier to the document. */
+  
+  // await axios.put(`/libraryDocuments/${libraryDocumentId}/formFields`);
 }
 
 let libraryDocumentId = "CBJCHBCAABAA7V0riaWVDHwrLaSkRddihs_aqME4QQuz";
 main(libraryDocumentId, true);
-
-
