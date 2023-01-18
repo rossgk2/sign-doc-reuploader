ad94087d7a5f14b277e0ea8ca219d0990c59ea67
Ross Grogan-Kaylor <rossgk2@gmail.com>
2023-01-18 13:47:07 -0500

    Cleaned up download.service.ts even more

--- sign-template-reuploader/src/app/services/download.service.ts
+++ sign-template-reuploader/src/app/services/download.service.ts
@@ -19,18 +19,18 @@ export class DownloadService {
 
   async getAllDocuments(bearerAuth: string): Promise<any> {
     const baseUrl = await this.urlGetterService.getApiBaseUriCommercial(bearerAuth);
-    return this.http.get(baseUrl + '/libraryDocuments', this.defaultHttpOptions(bearerAuth)).toPromise();
+    return this.http.get(`${baseUrl}/libraryDocuments`, this.defaultHttpOptions(bearerAuth)).toPromise();
   }
 
   /* Deprecated functions below. */
 
   async getDocument(documentId: string, bearerAuth: string): Promise<any> {
     const baseUrl = await this.urlGetterService.getApiBaseUriCommercial(bearerAuth);
-    return this.http.get(baseUrl + '/' + documentId, this.defaultHttpOptions(bearerAuth)).toPromise();
+    return this.http.get(`${baseUrl}/${documentId}`, this.defaultHttpOptions(bearerAuth)).toPromise();
   }
 
   async getFormFields(documentId: string, bearerAuth: string): Promise<any> {
     const baseUrl = await this.urlGetterService.getApiBaseUriCommercial(bearerAuth);
-    return this.http.get(baseUrl + '/' + documentId + '/' + 'formFields', this.defaultHttpOptions(bearerAuth)).toPromise();
+    return this.http.get(`{baseUrl}/${documentId}/formFields`, this.defaultHttpOptions(bearerAuth)).toPromise();
   }
 }
