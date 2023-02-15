7019bbbe3bac7cffcdf88b38d62438d6ac2d6ee9
Ross Grogan-Kaylor <rossgk2@gmail.com>
2023-02-15 15:11:26 -0500

    Deleted credentials file

--- src/app/settings/credentials.ts
+++
@@ -1,6 +0,0 @@
-export const Credentials = {
-  commercialIntegrationKey: '(This sensitive info has been removed by BFG repo cleaner)',
-  oAuthClientId: '(This sensitive info has been removed by BFG repo cleaner)',
-  oAuthClientSecret: '(This sensitive info has been removed by BFG repo cleaner)', // definitely don't expose this!
-  loginEmail: '(This sensitive info has been removed by BFG repo cleaner)'
-}
